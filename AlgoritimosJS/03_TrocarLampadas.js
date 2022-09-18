f9e36c2f59b9c863ca3dc4ce689e69bc3efd54a0
Fabio Macieira <fabiomacieira16@gmail.com>
2022-09-18 17:11:01 -0300

    Algoritmo para trocar lâmpadas

---
+++ AlgoritimosJS/03_TrocarLampadas.js
@@ -0,0 +1,9 @@
+var AtividadesDeCasa = ["estudar","almoçar","televisão", "dormir" ];
+
+function FimDeSemana(previsaoTempo ){
+    if( previsaoTempo == "fizer sol"){
+        console.log("vou à praia");
+    }else{
+        AtividadesDeCasa.map(atividade => atividade.)
+    }
+}
